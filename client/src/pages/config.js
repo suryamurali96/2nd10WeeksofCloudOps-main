00fd4a1d892bcaae97163015ef2ce574afab72a5
suryamurali96 <chesuryamurali@gmail.com>
2025-01-25 14:45:38 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://backend.suryamuralis.com";
+const API_BASE_URL = "http://api.suryamuralis.com";
 export default API_BASE_URL;
